802c54a8590d01c40058c76bcc445805ddbdb3f6
arvydas-tim <66131010+arvydas-tim@users.noreply.github.com>
2020-06-03 06:52:52 +0300

    Add files via upload

---
+++ VcsWebdriver/Drivers/CustomDrivers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace VcsWebdriver.Drivers
+{
+    public static class CustomDrivers
+    {
+        public static IWebDriver GetFirefoxDriver()
+        {
+            var Driver = new FirefoxDriver();
+            Driver.Manage().Window.Maximize();
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            return Driver;
+        }
+        public static IWebDriver GetChromeDriver()
+        {
+            var Driver = new ChromeDriver();
+            Driver.Manage().Window.Maximize();
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            return Driver;
+        }
+
+
+
+    }
+}
